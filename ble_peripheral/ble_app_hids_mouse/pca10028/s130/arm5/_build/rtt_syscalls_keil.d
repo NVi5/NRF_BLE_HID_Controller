5cc69b51bb901b3d0cfacb67eb9a6ae232b9ee9b
Jan Wołowiec <wolowiec.jan7@gmail.com>
2020-10-19 19:51:25 +0200

    Initial commit

---
+++ ble_peripheral/ble_app_hids_mouse/pca10028/s130/arm5/_build/rtt_syscalls_keil.d
@@ -0,0 +1,1 @@
+.\_build\rtt_syscalls_keil.o: D:\Electronics\Programs\KeilPacks\NordicSemiconductor\nRF_Drivers_External\1.2.1-2.alpha\segger_rtt\RTT_Syscalls_KEIL.c
---
+++ ble_peripheral/ble_app_hids_mouse/pca10028/s130/arm5/_build/segger_rtt.d
@@ -0,0 +1,4 @@
+.\_build\segger_rtt.o: RTE\nRF_Libraries\nRF51422_xxAC\SEGGER_RTT.c
+.\_build\segger_rtt.o: RTE\nRF_Libraries\nRF51422_xxAC\SEGGER_RTT.h
+.\_build\segger_rtt.o: RTE\nRF_Libraries\nRF51422_xxAC\SEGGER_RTT_Conf.h
+.\_build\segger_rtt.o: D:\Electronics\Programs\Keil\ARM\ARMCC\Bin\..\include\string.h
