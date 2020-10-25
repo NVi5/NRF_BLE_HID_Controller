4cd41fb0f15c40c9cc06d29179da9c1e5927ccaa
Jan Wołowiec <wolowiec.jan7@gmail.com>
2020-10-25 16:53:55 +0100

    Remove not working scripts

--- python_scripts/scan.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from bleak import discover
-
-async def run():
-    return await discover()
-
-loop = asyncio.get_event_loop()
-device_list = loop.run_until_complete(run())
-print(device_list)
